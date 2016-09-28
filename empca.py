76936bc027a8612a8adecb96eebda4f5c29637af
ArtsemZhuk <zhuk1996@gmail.com>
2016-09-28 21:18:57 +0300

    some graphs added

--- empca.py
+++ empca.py
@@ -22,6 +22,73 @@ class EMPCA(BaseEstimator, TransformerMixin):
         self.n_components = n_components
         self.n_iter = n_iter
 
+
+    def make_iteration_(self):
+        X = self.X_
+        n_samples, n_features = X.shape
+        n_components = self.n_components
+        x_sum = sum([dot(X[i], transpose(X[i])) for i in range(n_samples)])
+        W = self.components_.T
+        sigma = self.sigma_
+
+        # e-step
+        Wt = W.T
+        M = inv(dot(Wt, W) + sigma * np.eye(n_components))
+
+        Sigma = sigma * M
+
+        mu = dot(X, W)
+        mu = dot(mu, M.T)
+
+        # m-step
+        L = dot(transpose(X), mu)
+        R = dot(transpose(mu), mu) + n_samples * Sigma  # sum Ett
+
+        W_new = dot(L, inv(R))
+        W_new_t = transpose(W_new)
+
+        C = trace(dot(dot(W_new_t, W_new), R))
+        B = sum([dot(dot(mu[i], W_new_t), transpose(X[i])) for i in range(n_samples)])
+        A = x_sum
+        sigma_new = (A - 2 * B + C) / n_samples / n_features
+
+        # W = gram_schmidt(W_new, tr=True)
+        # sigma = sigma_new
+
+        self.sigma_ = sigma_new
+        self.components_ = gram_schmidt(W_new.T)
+
+
+    def init_(self, X):
+        X = check_array(X, dtype=[np.float64], ensure_2d=True, copy=True)
+
+        self.mean_ = normalize(X)
+        self.X_ = X
+
+        self.sigma_ = 1
+
+        n_samples, n_features = X.shape
+        n_components = self.n_components
+
+        self.components_ = np.empty([n_components, n_features], dtype=np.float64)
+        for i in range(n_components):
+            for j in range(n_features):
+                self.components_[i][j] = np.random.uniform(-1, 1)
+
+    def calc_ratio_(self):
+        X = self.X_
+        W = self.components_
+        Wl = W.dot(X.T).dot(X)
+        lambdas = []
+
+        for i in range(self.n_components):
+            l = np.linalg.norm(Wl[i])
+            lambdas.append(l)
+
+        x_sum = trace(X.dot(X.T))
+        self.lambdas_ = lambdas
+        self.explained_variance_ratio_ = lambdas / x_sum
+
     def fit(self, X, y=None):
         """
         Fit the model with X
@@ -29,61 +96,14 @@ class EMPCA(BaseEstimator, TransformerMixin):
         :param y: redundant
         :return: self
         """
-        X = check_array(X, dtype=[np.float64], ensure_2d=True)
 
-        n_samples, n_features = X.shape
-        n_components = self.n_components
-
-        self.mean_ = normalize(X)
-
-        sigma = 1
-
-        W = np.empty([n_features, n_components], dtype=np.float64)
-        for i in range(n_features):
-            for j in range(n_components):
-                W[i][j] = np.random.uniform(-1, 1)
-
-        x_sum = sum([dot(X[i], transpose(X[i])) for i in range(n_samples)])
+        self.init_(X)
 
         for _ in range(self.n_iter):
-            # e-step
-            Wt = W.T
-            M = inv(dot(Wt, W) + sigma * np.eye(n_components))
+            self.make_iteration_()
 
-            Sigma = sigma * M
+        self.calc_ratio_()
 
-            mu = dot(X, W)
-            mu = dot(mu, M.T)
-
-            # m-step
-            L = dot(transpose(X), mu)
-            R = dot(transpose(mu), mu) + n_samples * Sigma  #  sum Ett
-
-            W_new = dot(L, inv(R))
-            W_new_t = transpose(W_new)
-
-
-            C = trace(dot(dot(W_new_t, W_new), R))
-            B = sum([dot(dot(mu[i], W_new_t), transpose(X[i])) for i in range(n_samples)])
-            A = x_sum
-            sigma_new = (A - 2 * B + C) / n_samples / n_features
-
-            W = gram_schmidt(W_new, tr=True)
-            sigma = sigma_new
-
-        self.components_ = gram_schmidt(W.T)
-
-        W = self.components_
-        Wl = W.dot(X.T).dot(X)
-        lambdas = []
-
-        for i in range(n_components):
-            l = np.linalg.norm(Wl[i])
-            lambdas.append(l)
-
-        self.lambdas_ = lambdas
-        self.explained_variance_ratio_ = lambdas / x_sum
-        self.sigma_ = sigma
 
     def transform(self, X, y=None):
         """
@@ -92,9 +112,9 @@ class EMPCA(BaseEstimator, TransformerMixin):
         :param y: redundant
         :return:
         """
-        X = check_array(X, dtype=[np.float64], ensure_2d=True)
+        X = check_array(X, dtype=[np.float64], ensure_2d=True, copy=True)
         X -= self.mean_
-        return project_many(X, self.components_)
+        return X.dot(self.components_.T)
 
     def fit_transform(self, X, y=None):
         Xc = copy(X)
@@ -109,7 +129,7 @@ if __name__ == '__main__':
 
     empca = EMPCA(n_components=5, n_iter=300)
     empca.fit(X)
-    #print("transformed\n", empca.transform(X))
+    print("transformed\n", empca.transform(X))
     #print("components\n", empca.components_)
     print("ratio:\n", empca.explained_variance_ratio_)
     #print(empca.transform(X))
